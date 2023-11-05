9583f9b307960ffc2efb987b3d2e02714094bf99
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-11-05 03:54:48 +0100

    start writing requirements of todo-list module

--- src/todo-list.js
+++ src/todo-list.js
@@ -1,1 +1,11 @@
 import { ToDoItem } from "./todo-item";
+
+// a full todo list;
+// each todo list item must have
+//  - a title value;
+
+/*
+
+creating new todos, setting todos as complete, changing todo priority
+
+*/
